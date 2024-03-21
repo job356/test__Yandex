28ab989ec77858848600e92849dcd25ba7e71a10
Nikita Dobryakov <nik.dobryakov.96@mail.ru>
2024-03-22 02:19:19 +0300

    feat: last changes

--- slider-steps-mobile.js
+++ slider-steps-mobile.js
@@ -17,3 +17,53 @@ document.querySelector('.arrow-contol_left').addEventListener('click', function(
     stepsBoard.style.left = -stepsBoardOffset + 'px';
 });
 
+
+const buttonLeft = document.querySelector('.arrow-contol_left');
+const buttonRight = document.querySelector('.arrow-contol_right');
+const circles = document.querySelectorAll('.slide-value__circle');
+let currentIndicate = 0;
+
+function updateCurrentIndicate(index) {
+  if (index < 0) {
+    index = circles.length - 1;
+  }
+  if (index > circles.length - 1) {
+    index = 0;
+  }
+  return index;
+}
+
+buttonLeft.addEventListener('click', () => {
+  currentIndicate = updateCurrentIndicate(currentIndicate - 1);
+  circles.forEach((circle, i) => {
+    if (i === currentIndicate) {
+      circle.style.backgroundColor = '#313131';
+    } else {
+      circle.style.backgroundColor = '#D9D9D9';
+    }
+  });
+});
+
+buttonRight.addEventListener('click', () => {
+  currentIndicate = updateCurrentIndicate(currentIndicate + 1);
+  circles.forEach((circle, i) => {
+    if (i === currentIndicate) {
+      circle.style.backgroundColor = '#313131';
+    } else {
+      circle.style.backgroundColor = '#D9D9D9';
+    }
+  });
+});
+
+ circles.forEach((circle, i) => {
+    circle.addEventListener('click', () => {
+      currentIndicate = i;
+      circles.forEach((c, j) => {
+        if (i === j) {
+          c.style.backgroundColor = '#313131';
+        } else {
+          c.style.backgroundColor = '#D9D9D9';
+        }
+      });
+    });
+  });
